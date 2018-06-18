8ff5e0c465cb5898082570890efb0913f7c9a4c8
Kizito Njoku <nkizito57@gmail.com>
2018-06-17 23:35:10 -0500

    sticky nav js null error removed

--- JS/index.js
+++ JS/index.js
@@ -1,33 +1,37 @@
 function index() {
     "use strict";
     if (screen.width <= 800) {
-            window.location.replace("MobilePort/index.html");
-        }
-var nav =  document.getElementById("navv");
-var nav2 = document.getElementById("nav2");
+        window.location.replace("MobilePort/index.html");
+    }
+    var nav = document.querySelector("nav");
+    var nav2 = document.querySelector("#nav2");
 
-    
- function fixed() {
-        if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-            //            grabs a class type selector and adds it to the Id
-            nav.className = "fixedNav";
-            
 
+    function fixed() {
+
+        if (nav2!= true) {
+            if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+                //            grabs a class type selector and adds it to the Id
+                nav.className = "fixedNav";
+
+
+            } else {
+                nav.className = "";
+
+            }
         } else {
-            nav.className = "";
-             
-        }
-        
-        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            //            grabs a class type selector and adds it to the Id
-            nav2.className = "fixedNav";
-            
+            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                //            grabs a class type selector and adds it to the Id
+                nav2.className = "fixedNav";
 
-        } else {
-            nav2.className = "";
-             
+
+            } else {
+                nav2.className = "";
+
+            }
         }
-    } 
+
+    }
     // slide names for images
     var slide = document.getElementById("img1");
     var slideB = document.getElementById("img3");
@@ -38,7 +42,7 @@ var nav2 = document.getElementById("nav2");
         if (document.body.scrollTop > 980 || document.documentElement.scrollTop > 980) {
             slide.className = "slide";
             slide.style.opacity = 1;
-        } 
+        }
     }
     window.addEventListener("scroll", slide1);
 
@@ -67,8 +71,9 @@ var nav2 = document.getElementById("nav2");
         }
     }
     window.addEventListener("scroll", slide3);
+
     function slide4() {
-        if (document.body.scrollTop > 2440|| document.documentElement.scrollTop > 2440) {
+        if (document.body.scrollTop > 2440 || document.documentElement.scrollTop > 2440) {
             slideD.className = "slide";
             slideD.style.opacity = 1;
         } else {
@@ -80,56 +85,11 @@ var nav2 = document.getElementById("nav2");
     }
     window.addEventListener("scroll", slide4);
     window.addEventListener("scroll", fixed);
-     function submition(){
+
+    function submition() {
         document.forms["formSub"].action = 'submit.php';
         document.forms["formSub"].submit();
-     }
-    
-// display functions
-//    function display() {
-//        document.getElementById("li").hidden = true;
-//        var button1 = document.getElementById("button1");
-//        button1.innerHTML = "show";
-//
-//        function show() {
-//            document.getElementById("li").hidden = false;
-//            button1.innerHTML = "hide";
-//
-//            function hide() {
-//                document.getElementById("li").hidden = true;
-//                button1.innerHTML = "show";
-//                display();
-//            }
-//            button1.addEventListener("click", hide);
-//        }
-//        button1.addEventListener("click", show);
-//
-//    }
-//    display();
-//
-//    // create a button
-//    var button4 = document.createElement("button");
-//    button4.innerHTML = "clear";
-//    //set button in body with appendChilds
-//    document.body.appendChild(button4).hidden = true;
-//    button4.style.margin = "30px 0 0 700px";
-//    button4.id = "Clearing";
-//    button4 = document.getElementById("Clearing");
-//
-//    document.getElementById("go").addEventListener("click", function output() {
-//
-//        var displayInput = document.getElementById("userIn").value;
-//        document.getElementById("read").innerHTML += displayInput + "<br>";
-//
-//        button4.hidden = false;
-//    });
-//
-//    function gg() {
-//        document.getElementById("read").innerHTML = "";
-//        button4.hidden = true;
-//    }
-//
-//    button4.addEventListener("click", gg);
+    }
 
 
 }
