8311910cb52d369a4c3052611d910beeb8db3505
Kizito Njoku <nkizito57@gmail.com>
2018-10-28 22:13:12 -0500

    Change to anime hamnav menu

--- M/HTML/Contact.php
+++ M/HTML/Contact.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <script src="https://use.fontawesome.com/b563a0f3f3.js"></script>
     <meta name="viewport" content="initial-scale=1.0">
+    <script src="../assets/JS/index.js"></script>
 </head>
 
 <body>
@@ -57,9 +58,7 @@
 
     <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
     <script>
-        $("#item").click(function() {
-            $("#subitem").slideToggle(500);
-        });
+       nav();
     </script>
 </body>
 
--- M/assets/JS/index.js
+++ M/assets/JS/index.js
@@ -1,95 +1,123 @@
 function index() {
-    "use strict";
-    
-    // NavBar
-    
-     var navburger = {
-         divA: $("#item div:nth-child(1)"),
-         divB: $("#item div:nth-child(2)"),
-         divC: $("#item div:nth-child(3)")
-     }
-    $("#item").click(function () {
-        $("#item").toggleClass("cirHamNav");
-        navburger.divC.toggleClass("third");
-        navburger.divA.toggleClass("first");
-        navburger.divB.toggleClass("second");
-        $("#subitem").slideToggle(500);
-    });
-    // removes greeting text
-    setTimeout(function () {
-        var greetings = document.querySelectorAll("#TreeImg div");
-        for (var remove = 0; remove < greetings.length; remove++) {
-            greetings[remove].innerHTML = "";
-        }
-    }, 4000);
+  "use strict";
 
-    // slide names for assets/images
-    var slide = document.getElementById("img1");
-    var slideB = document.getElementById("img3");
-    var slideC = document.getElementById("img4");
-    var slideD = document.getElementById("img5");
-    var games = ["url(../assets/images/app.JPG)", "url(../assets/images/colors.JPG)", "url(../assets/images/Circles.png)"];
-    var gameimgs = 0;
-    slideC.style.height="104px";
-    function slide1() {
-        if (document.body.scrollTop > 380 || document.documentElement.scrollTop > 380) {
-            slide.className = "slide";
-            slide.style.opacity = 1;
-        } 
+  // NavBar
+
+  var navburger = {
+    divA: $("#item div:nth-child(1)"),
+    divB: $("#item div:nth-child(2)"),
+    divC: $("#item div:nth-child(3)")
+  };
+  $("#item").click(function() {
+    $("#item").toggleClass("cirHamNav");
+    navburger.divC.toggleClass("third");
+    navburger.divA.toggleClass("first");
+    navburger.divB.toggleClass("second");
+    $("#subitem").slideToggle(500);
+  });
+  // removes greeting text
+  setTimeout(function() {
+    var greetings = document.querySelectorAll("#TreeImg div");
+    for (var remove = 0; remove < greetings.length; remove++) {
+      greetings[remove].innerHTML = "";
     }
+  }, 4000);
 
-    function slide2() {
-        if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
-            slideB.className = "slide";
-            slideB.style.opacity = 1;
-        } else {
-            slideB.className = "slideback";
-            slideB.style.opacity = 0;
-        }
+  // slide names for assets/images
+  var slide = document.getElementById("img1");
+  var slideB = document.getElementById("img3");
+  var slideC = document.getElementById("img4");
+  var slideD = document.getElementById("img5");
+  var games = [
+    "url(../assets/images/app.JPG)",
+    "url(../assets/images/colors.JPG)",
+    "url(../assets/images/Circles.png)"
+  ];
+  var gameImgs = 0;
+  slideC.style.height = "104px";
+  function slide1() {
+    if (
+      document.body.scrollTop > 380 ||
+      document.documentElement.scrollTop > 380
+    ) {
+      slide.className = "slide";
+      slide.style.opacity = 1;
     }
-    var gaming = document.getElementById("gamer");
-    function slide3() {
-        if (document.body.scrollTop > 1400 || document.documentElement.scrollTop > 1400) {
-            slideC.className = "slide";
-            slideC.style.opacity = 1;
-        } else {
-            slideC.className = "slideback";
-            slideC.style.opacity = 0;
+  }
 
-
-        }
+  function slide2() {
+    if (
+      document.body.scrollTop > 900 ||
+      document.documentElement.scrollTop > 900
+    ) {
+      slideB.className = "slide";
+      slideB.style.opacity = 1;
+    } else {
+      slideB.className = "slideback";
+      slideB.style.opacity = 0;
     }
-    // Changes app images
-    var gamer = ["game/MatchCards/matchCards.html",
-"game/ColorGame/Colors.html", "../Challenges/Circles/Circles.html"];
-     setInterval(function(){
-         gameimgs++;
-        if (gameimgs > games.length -1){
-            gameimgs = 0;
-        }
-         slideC.style.backgroundImage = games[gameimgs];
-          gaming.setAttribute("href", gamer[gameimgs]);
-    }, 3000);
-
-    function slide4() {
-        if (document.body.scrollTop > 1520 || document.documentElement.scrollTop > 1520) {
-            slideD.className = "slide";
-            slideD.style.opacity = 1;
-        } else {
-            slideD.className = "slideback";
-            slideD.style.opacity = 0;
-
-
-        }
+  }
+  var gaming = document.getElementById("gamer");
+  function slide3() {
+    if (
+      document.body.scrollTop > 1400 ||
+      document.documentElement.scrollTop > 1400
+    ) {
+      slideC.className = "slide";
+      slideC.style.opacity = 1;
+    } else {
+      slideC.className = "slideback";
+      slideC.style.opacity = 0;
     }
-    var sliders = [slide1, slide2, slide3, slide4];
-
-    function sliding() {
-        for (var i = 0; i <= sliders.length; i++) {
-            window.addEventListener("scroll", sliders[i]);
-        }
+  }
+  // Changes app images
+  var gamer = [
+    "game/MatchCards/matchCards.html",
+    "game/ColorGame/Colors.html",
+    "../Challenges/Circles/Circles.html"
+  ];
+  setInterval(function() {
+    gameimgs++;
+    if (gameimgs > games.length - 1) {
+      gameimgs = 0;
     }
+    slideC.style.backgroundImage = games[gameImgs];
+    gaming.setAttribute("href", gamer[gameImgs]);
+  }, 3000);
 
-    sliding();
+  function slide4() {
+    if (
+      document.body.scrollTop > 1520 ||
+      document.documentElement.scrollTop > 1520
+    ) {
+      slideD.className = "slide";
+      slideD.style.opacity = 1;
+    } else {
+      slideD.className = "slideback";
+      slideD.style.opacity = 0;
+    }
+  }
+  var sliders = [slide1, slide2, slide3, slide4];
 
+  function sliding() {
+    for (var i = 0; i <= sliders.length; i++) {
+      window.addEventListener("scroll", sliders[i]);
+    }
+  }
+
+  sliding();
+}
+function nav() {
+  var navburger = {
+    divA: $("#item div:nth-child(1)"),
+    divB: $("#item div:nth-child(2)"),
+    divC: $("#item div:nth-child(3)")
+  };
+  $("#item").click(function() {
+    $("#item").toggleClass("cirHamNav");
+    navburger.divC.toggleClass("third");
+    navburger.divA.toggleClass("first");
+    navburger.divB.toggleClass("second");
+    $("#subitem").slideToggle(500);
+  });
 }
